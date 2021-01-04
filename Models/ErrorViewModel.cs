0bccbd9afb3870170a5f06ad5374a0690a099d25
Hieu Dong <donghuuhieu1520@gmail.com>
2021-01-04 22:12:59 +0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MVCReview.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
